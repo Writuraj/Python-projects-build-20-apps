7fafec80eb9a95ac4522b621de5720521510e242
writu <dkwrituraj@gmail.com>
2025-10-05 23:26:06 +0600

    Implement of an edit button

--- gui.py
+++ gui.py
@@ -7,9 +7,15 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos("test.txt"),
                       key='todos', enable_events=True, size=(45,10))
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("exit")
+
 
 window = sg.Window("My To-Do App",
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout=[[label],
+                           [input_box, add_button],
+                           [list_box, edit_button,complete_button],
+                           [exit_button]],
                    font=('Helvetica', 12))
 
 while True:
@@ -24,21 +30,28 @@ while True:
             todos.append(new_todo)
             functions.write_todos("test.txt", todos)
             window['todos'].update(todos)
+        case "Complete":
+            todo_to_complete=values['todos'][0]
+            todos = functions.get_todos("test.txt")
+            todos.remove(todo_to_complete)
+            functions.write_todos("test.txt", todos)
+            window['todos'].update(values=todos)
 
+            window['todo'].update(value="")
         case "Edit":
-            try:
-                todo_to_edit = values['todos'][0]
-                new_todo = values['todo']
 
-                todos = functions.get_todos("test.txt")
-                index = todos.index(todo_to_edit)
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
 
-                todos[index] = new_todo + "\n"
-                functions.write_todos("test.txt", todos)
+            todos = functions.get_todos("test.txt")
+            index = todos.index(todo_to_edit)
 
-                window['todos'].update(values=todos)
-            except IndexError:
-                pass
+            todos[index] = new_todo + "\n"
+            functions.write_todos("test.txt", todos)
+
+            window['todos'].update(values=todos)
+        case 'exit':
+            break
 
         case "todos":
             window['todo'].update(value=values['todos'][0].strip())
