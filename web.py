522ab8a5bcc754c1a6d2e4eac36296e002af9395
writu <dkwrituraj@gmail.com>
2025-10-08 22:59:06 +0600

    adding an complete feature in webapp

--- web.py
+++ web.py
@@ -15,8 +15,14 @@ st.subheader("This is my todo app")
 st.write("This app is to increase your productivity")
 st.checkbox("Buy groceries")
 
-for todo in todos:
-    st.checkbox(todo)
+for index,todo in enumerate(todos):
+    checkbox = st.checkbox(todo,key=todo)
+    if checkbox:
+        todos.pop(index)
+        f.write_todos("test.txt",todos)
+        del st.session_state[todo]
+        st.rerun()
+
 
 st.text_input(label=" ",placeholder="Enter a To do...",
               on_change=add_todo,key='new_todo')
